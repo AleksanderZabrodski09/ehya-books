b5fa1f6d0a8e72dcd88da1d74b826aedb68582f3
AlesanderZabrodski09 <zabrodski666@gmail.com>
2022-02-02 19:20:33 +0100

    create: add section category

--- js/main.js
+++ js/main.js
@@ -1,7 +1,25 @@
 var likeHeartButton = document.querySelector(".like-heart");
-likeHeartButton.addEventListener("click", function () {
+  likeHeartButton.addEventListener("click", function (event) {
   this.classList.toggle("clicked");
         // likeHeartButton.addEventListener("click", function (event) {
         // console.log((event.target.style.fill = "red"));
-});
+  });
 
+var categorySlider = new Swiper('.category-slider', {
+    loop: true,
+
+  navigation: {
+    nextEl: '.category-slider__button--next',
+    prevEl: '.category-slider__button--prev'
+  },
+  // effect: "flip",
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+
+  slidesPerView: 4,
+  spaceBetween: 26,
+});
+  
+  
