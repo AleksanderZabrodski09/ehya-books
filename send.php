4ff14dee86931976ca52b4c3833fc5176e3f983d
AlesanderZabrodski09 <zabrodski666@gmail.com>
2022-04-02 21:30:42 +0200

    create: add send.php

---
+++ send.php
@@ -0,0 +1,64 @@
+<?php
+// Файлы phpmailer
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+// Переменные, которые отправляет пользователь
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+$email = $_POST['email'];
+if (isset($_POST[subscribe])){
+$body = "
+<h2>New letter</h2>
+email: $email";
+} else {
+  $body = "
+  <h2>New letter</h2>
+  <b>Name:</b> $name<br>
+  <b>Email:</b> $email<br><br>
+  <b>Message:</b><br>$message
+  ";
+};
+
+// Формирование самого письма
+$title = "New appeal from Ehya - books";
+
+// Настройки PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+    $mail->isSMTP();   
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth   = true;
+    $mail->SMTPDebug = 2;
+    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+
+    // Настройки вашей почты
+    $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
+    $mail->Username   = 'web.wellcome.service@gmail.com'; // Логин на почте
+    $mail->Password   = 'za9045696'; // Пароль на почте
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+    $mail->setFrom('web.wellcome.service@gmail.com', 'Aleksander Zabrodski'); // Адрес самой почты и имя отправителя
+
+    // Получатель письма
+    $mail->addAddress('zabrodski666@gmail.com');  
+
+// Отправка сообщения
+$mail->isHTML(true);
+$mail->Subject = $title;
+$mail->Body = $body;    
+
+// Проверяем отравленность сообщения
+if ($mail->send()) {$result = "success";} 
+else {$result = "error";}
+
+} catch (Exception $e) {
+    $result = "error";
+    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
+
+// Отображение результата
+echo json_encode(["result" => $result, "resultfile" => $rfile, "status" => $status]);
+// header('Location: thankyou.html');
